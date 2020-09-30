ab55bf5c225e6772a86ee53b0558ddcda5c5a3d3
Ugo Emekauwa <51210375+ugo-emekauwa@users.noreply.github.com>
2020-09-30 14:38:31 -0400

    Add files via upload

--- create_tenant_user.ps1
+++ create_tenant_user.ps1
@@ -1,14 +1,22 @@
-# Create Tenant User in Windows Azure Pack (WAP) Script, written by Ugo Emekauwa (uemekauw@cisco.com or uemekauwa@gmail.com)
+# Create Tenant User in Windows Azure Pack Script v2, written by Ugo Emekauwa (uemekauw@cisco.com or uemekauwa@gmail.com)
 
-# Import Required Modules for WAP
+# Start Script
+Write-Output "`r`n$(Get-Date) - Starting 'Create Tenant User in Windows Azure Pack Script v2'." | Out-File -Append "c:\scripts\create_tenant_user.log"
+
+# Import Required Modules for SCVMM and WAP
+Write-Output "$(Get-Date) - Importing required modules for SCVMM and WAP." | Out-File -Append "c:\scripts\create_tenant_user.log"
+Import-Module virtualmachinemanager
+Import-Module virtualmachinemanagercore
 Import-Module MgmtSvcAdmin
 
-# Set Administrator Credentials for Active Directory and WAP
+# Set Administrator Credentials for SCVMM and WAP
+Write-Output "$(Get-Date) - Setting Administrator credentials for SCVMM and WAP." | Out-File -Append "c:\scripts\create_tenant_user.log"
 $Admin = "domain\Administrator"
 $Password = "Password" | ConvertTo-SecureString -AsPlainText -Force
 $AdminCred = New-Object System.Management.Automation.PSCredential($Admin,$Password)
 
 # Create User in Active Directory
+Write-Output "$(Get-Date) - Creating user in Active Directory." | Out-File -Append "c:\scripts\create_tenant_user.log"
 $ADUserFullName = "Gerald Lawson"
 $ADUserJobTitle = "Electronic Engineer"
 $ADUserGivenName = "Gerald"
@@ -20,26 +28,53 @@ $ADUserPassword = "Password"
 New-ADUser -Name $ADUserFullName -Title $ADUserJobTitle -GivenName $ADUserGivenName -Surname $ADUserSurname -SamAccountName $ADUserSAM -UserPrincipalName $ADUserUPN -EmailAddress $ADUserEmail -Path "CN=Users,DC=domain,DC=org" -AccountPassword(ConvertTo-SecureString $ADUserPassword -AsPlainText -Force) -Enabled $true
 
 # Set WAP Environment Specific Variables
+Write-Output "$(Get-Date) - Setting WAP environment specific variables." | Out-File -Append "c:\scripts\create_tenant_user.log"
 $WAPServer = "wap1.domain.org"
 
 # Establish Connection to WAP
+Write-Output "$(Get-Date) - Establishing connection to $($WAPServer)." | Out-File -Append "c:\scripts\create_tenant_user.log"
 $AdminURI = "https://" + $WAPServer + ":30004"
 $AuthSite = "https://" + $WAPServer + ":30072"
 $ClientRealm = "http://azureservices/AdminSite"
 $Token = Get-MgmtSvcToken -Type Windows -AuthenticationSite $AuthSite -ClientRealm $ClientRealm -User $AdminCred -DisableCertificateValidation
 
 # Set WAP Plan Variables
+Write-Output "$(Get-Date) - Setting WAP plan variables for AD user $($ADUserSAM)." | Out-File -Append "c:\scripts\create_tenant_user.log"
 $PlanID = "Sample_Plan_ID"
 $SubFN = "Sample_Plan_ID_Friendly_Name"
 
 # Define Tenant User for WAP
+Write-Output "$(Get-Date) - Defining tenant user for WAP for AD user $($ADUserSAM)." | Out-File -Append "c:\scripts\create_tenant_user.log"
 $WAPUser = Get-ADUser $ADUserSAM
 
 # Create Tenant User in WAP
-Add-MgmtSvcUser -AdminUri $AdminURI -Token $Token -Name $WAPUser.UserPrincipalName -Email $WAPUser.UserPrincipalName -State 'Active'
+Write-Output "$(Get-Date) - Creating tenant user in WAP for AD user $($ADUserSAM)." | Out-File -Append "c:\scripts\create_tenant_user.log"
+$NewWAPUser = Add-MgmtSvcUser -AdminUri $AdminURI -Token $Token -Name $WAPUser.UserPrincipalName -Email $WAPUser.UserPrincipalName -State 'Active'
 
 # Subscribe Tenant User to WAP Plan
-Add-MgmtSvcSubscription -AdminUri $AdminURI -Token $Token -AccountAdminLiveEmailId $WAPUser.UserPrincipalName -AccountAdminLivePuid $WAPUser.UserPrincipalName -PlanId $PlanID -FriendlyName $SubFN -DisableCertificateValidation
+Write-Output "$(Get-Date) - Subscribing WAP tenant user $($ADUserSAM) to WAP plan $($SubFN)." | Out-File -Append "c:\scripts\create_tenant_user.log"
+$NewWAPUserSub = Add-MgmtSvcSubscription -AdminUri $AdminURI -Token $Token -AccountAdminLiveEmailId $WAPUser.UserPrincipalName -AccountAdminLivePuid $WAPUser.UserPrincipalName -PlanId $PlanID -FriendlyName $SubFN -DisableCertificateValidation
+
+# Set SCVMM Environment Specific Variables
+Write-Output "$(Get-Date) - Setting SCVMM environment specific variables." | Out-File -Append "c:\scripts\create_tenant_user.log"
+$SCVMMServer = "scvmm1.domain.org"
+
+# Establish Connection to SCVMM
+Write-Output "$(Get-Date) - Establishing connection to $($SCVMMServer)." | Out-File -Append "c:\scripts\create_tenant_user.log"
+Get-SCVMMServer -ComputerName $SCVMMServer -Credential $AdminCred
+
+# Determine SCVMM User Role Name for New WAP User
+Write-Output "$(Get-Date) - Determining the SCVMM user role name for WAP tenant user $($ADUserSAM)." | Out-File -Append "c:\scripts\create_tenant_user.log"
+$SCUserRoleName = "$($NewWAPUser.Email)_$($NewWAPUserSub.SubscriptionID)"
+
+# Retrieve SCVMM User Role
+Write-Output "$(Get-Date) - Retrieving the SCVMM user role named $($SCUserRoleName)." | Out-File -Append "c:\scripts\create_tenant_user.log"
+$GetSCUserRole = Get-SCUserRole -Name $SCUserRoleName
+
+# Add AD User to SCVMMM User Role
+Write-Output "$(Get-Date) - Adding AD user $($ADUserSAM) to the SCVMM user role named $($SCUserRoleName)." | Out-File -Append "c:\scripts\create_tenant_user.log"
+Set-SCUserRole -UserRole $GetSCUserRole -AddMember $ADUserSAM
 
 # Exit Script
+Write-Output "$(Get-Date) - Exiting script.`r`n" | Out-File -Append "c:\scripts\create_tenant_user.log"
 Exit
--- delete_tenant_user.ps1
+++ delete_tenant_user.ps1
@@ -1,53 +1,73 @@
-# Delete Tenant User in Windows Azure Pack (WAP) Script, written by Ugo Emekauwa (uemekauw@cisco.com or uemekauwa@gmail.com)
+# Delete Tenant User in Windows Azure Pack Script v2, written by Ugo Emekauwa (uemekauw@cisco.com or uemekauwa@gmail.com)
 
-# Import Required Modules for WAP and System Center Virtual Machine Manager (SCVMM)
-Import-Module MgmtSvcAdmin
+# Start Script
+Write-Output "`r`n$(Get-Date) - Starting 'Delete Tenant User in Windows Azure Pack Script v2'." | Out-File -Append "c:\scripts\delete_tenant_user.log"
+
+# Import Required Modules for SCVMM and WAP
+Write-Output "$(Get-Date) - Importing required modules for SCVMM and WAP." | Out-File -Append "c:\scripts\delete_tenant_user.log"
 Import-Module virtualmachinemanager
 Import-Module virtualmachinemanagercore
+Import-Module MgmtSvcAdmin
 
-# Set Administrator Credentials for Active Directory, WAP and SCVMM
+# Set Administrator Credentials for Active Directory, SCVMM and WAP
+Write-Output "$(Get-Date) - Setting Administrator credentials for Active Directory, SCVMM and WAP." | Out-File -Append "c:\scripts\delete_tenant_user.log"
 $Admin = "domain\Administrator"
 $Password = "Password" | ConvertTo-SecureString -AsPlainText -Force
 $AdminCred = New-Object System.Management.Automation.PSCredential($Admin,$Password)
 
-# Identify Tenant User's Sam Account Name from Active Directory
+# Identify Tenant User's SAM Account Name from Active Directory
+Write-Output "$(Get-Date) - Identifying tenant user's SAM account name from Active Directory." | Out-File -Append "c:\scripts\delete_tenant_user.log"
 $ADUserSAM = "sampleuser"
 
-# Set WAP Environment Specific Variables
-$WAPServer = "wap1.domain.org"
-
-# Establish Connection to WAP
-$AdminURI = "https://" + $WAPServer + ":30004"
-$AuthSite = "https://" + $WAPServer + ":30072"
-$ClientRealm = "http://azureservices/AdminSite"
-$Token = Get-MgmtSvcToken -Type Windows -AuthenticationSite $AuthSite -ClientRealm $ClientRealm -User $AdminCred -DisableCertificateValidation
-
-# Define Tenant User for WAP
-$WAPUser = Get-ADUser $ADUserSAM
-
-# Delete Tenant User and Subscription in WAP
-Remove-MgmtSvcUser -Name $WAPUser.UserPrincipalName -AdminUri $AdminURI -Token $Token -DeleteSubscriptions -DisableCertificateValidation -Confirm:$false
+# Set SCVMM Environment Specific Variables
+Write-Output "$(Get-Date) - Setting SCVMM environment specific variables." | Out-File -Append "c:\scripts\delete_tenant_user.log"
+$SCVMMServer = "scvmm1.domain.org"
 
 # Establish Connection to SCVMM
-Get-SCVMMServer -ComputerName scvmm1.domain.org -Credential $AdminCred
+Write-Output "$(Get-Date) - Establishing connection to $($SCVMMServer)." | Out-File -Append "c:\scripts\delete_tenant_user.log"
+Get-SCVMMServer -ComputerName $SCVMMServer -Credential $AdminCred
 
-# Define Tenant User for SCVMM
+# Define Tenant User in SCVMM
+Write-Output "$(Get-Date) - Defining tenant user in SCVMM for AD user $($ADUserSAM)." | Out-File -Append "c:\scripts\delete_tenant_user.log"
 $SCVMMUser = $ADUserSAM
 
-# Delete Tenant User Resources in SCVMM
+# Delete Tenant User Cloud Resources in SCVMM
+Write-Output "$(Get-Date) - Deleting tenant user cloud resources in SCVMM for AD user $($ADUserSAM)." | Out-File -Append "c:\scripts\delete_tenant_user.log"
 $User_Resources = Get-CloudResource | Where-Object Owner -Match $SCVMMUser
 ForEach ($User_Resource in $User_Resources)
 { Remove-CloudResource -CloudResource $User_Resource }
 
 # Delete Tenant User Owned VMs in SCVMM
+Write-Output "$(Get-Date) - Deleting tenant user owned VMs in SCVMM for AD user $($ADUserSAM)." | Out-File -Append "c:\scripts\delete_tenant_user.log"
 $Owned_VMs = Get-SCVirtualMachine | Where-Object Owner -Match $SCVMMUser
 ForEach ($Owned_VM in $Owned_VMs)
 { Stop-SCVirtualMachine -VM $Owned_VM -Force }
 ForEach ($Owned_VM in $Owned_VMs)
 { Remove-SCVirtualMachine -VM $Owned_VM }
 
+# Set WAP Environment Specific Variables
+Write-Output "$(Get-Date) - Setting WAP environment specific variables." | Out-File -Append "c:\scripts\delete_tenant_user.log"
+$WAPServer = "wap1.domain.org"
+
+# Establish Connection to WAP
+Write-Output "$(Get-Date) - Establishing connection to $($WAPServer)." | Out-File -Append "c:\scripts\delete_tenant_user.log"
+$AdminURI = "https://" + $WAPServer + ":30004"
+$AuthSite = "https://" + $WAPServer + ":30072"
+$ClientRealm = "http://azureservices/AdminSite"
+$Token = Get-MgmtSvcToken -Type Windows -AuthenticationSite $AuthSite -ClientRealm $ClientRealm -User $AdminCred -DisableCertificateValidation
+
+# Define Tenant User for WAP
+Write-Output "$(Get-Date) - Defining tenant user for WAP for AD user $($ADUserSAM)." | Out-File -Append "c:\scripts\delete_tenant_user.log"
+$WAPUser = Get-ADUser $ADUserSAM
+
+# Delete Tenant User and Subscription in WAP
+Write-Output "$(Get-Date) - Deleting tenant user and subscription in WAP for AD user $($ADUserSAM)." | Out-File -Append "c:\scripts\delete_tenant_user.log"
+Remove-MgmtSvcUser -Name $WAPUser.UserPrincipalName -AdminUri $AdminURI -Token $Token -DeleteSubscriptions -DisableCertificateValidation -Confirm:$false
+
 # Delete Tenant User in Active Directory
+Write-Output "$(Get-Date) - Deleting tenant user $($ADUserSAM) in Active Directory." | Out-File -Append "c:\scripts\delete_tenant_user.log"
 Remove-ADUser -Identity $ADUserSAM -Credential $AdminCred -Confirm:$false
 
 # Exit Script
+Write-Output "$(Get-Date) - Exiting script.`r`n" | Out-File -Append "c:\scripts\delete_tenant_user.log"
 Exit
